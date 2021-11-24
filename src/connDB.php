a1a51e92663d04d9009f088add0db5facd4bcfa7
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2021-11-23 22:33:42 -0500

    Update connDB.php

--- src/connDB.php
+++ src/connDB.php
@@ -5,13 +5,10 @@ $DBName = "Vaccines";//name of your database
 $SQL_USER = "Sytivawheeler";//SQL user
 $SQL_PASS = "Supersonicspeed21";//SQL password
 
-$connectionOptions = array($DBName,$SQL_USER,$SQL_PASS)
-
 try{
     $dbh = new PDO("sqlsrv:server = tcp:".$SrvName.",1433; Database = ".$DBName, $SQL_USER, $SQL_PASS);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     exit("DB Connection Failed: ".$e->getMessage());
 }
-
 ?>
